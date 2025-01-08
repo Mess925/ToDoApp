4f19bd50f7404bf22c977ebda29ba5fab168ae27
Han Min Thant <hanminthant222@gmail.com>
2025-01-08 20:42:14 +0800

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Messs  on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Messs  on 8/1/25.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
